a9e2235b02d1d7d4174ec50ce0c86251c4bdf509
Lord Friky <24846044+lordfriky@users.noreply.github.com>
2023-08-08 23:25:56 -0600

    WIP update

--- front/script.js
+++ front/script.js
@@ -1,53 +1,84 @@
-let datosFirma = [];
-
 // Generamos el canvas de la firma
-const canvas = document.querySelector("canvas");
-const firma = new SignaturePad(canvas);
+const canvas = document.getElementById("canvasFirma");
+const firma = new SignaturePad(canvas, {
+  dotSize: 1.0,
+  minWidth: 0.5,
+  maxWidth: 1.0
+});
 
-function limpiarCanvas(){
-  return firma.clear();
-}
+let limpiarCanvas = () => {
+  canvas.style.borderColor = "#dfe2e6";
+  firma.clear();
+};
 
-function guardarFirma(){
-  return datosFirma = firma.toData();
-}
-
-function abrirFirma(){
-  return firma.fromData(datosFirma);
-}
+let toogleCircle = (status) => {
+  const circle = document.getElementById("circle-loader");
+  circle.classList.add(status);
+};
 
 // Agregamos validación a los campos del formulario al cargar la página
 (() => {
   'use strict';
+
   window.addEventListener('load', function() {
   
   // Obetenemos todos los campos del formulario
   var campos = document.getElementsByClassName('form-control')
   
-  // Iteramos sobre todos y les agregamos una función cuando terminen de escribir
+  // Iteramos sobre todos y les agregamos una función al hacer click y cuando terminen de escribir
   var validar = Array.prototype.filter.call(campos, function(campo) {
-  
-      campo.addEventListener('blur', function(event) {
-        // Limpiamos las etiquetas si tenían
-        campo.classList.remove('is-invalid')
-        campo.classList.remove('is-valid')
-      
-        if (!campo.checkValidity()) {
-          campo.classList.add('is-invalid')
-        } else {
-          campo.classList.add('is-valid')
-        }
-      }, false);
+    campo.addEventListener('click', (event) => {
+      campo.classList.remove('is-invalid');
+      campo.classList.remove('is-valid');
+    }, false);
+
+    campo.addEventListener('blur', function(event) {
+      campo.classList.remove('is-invalid');
+      campo.classList.remove('is-valid');
+
+      if (!campo.checkValidity()) {
+        campo.classList.add('is-invalid')
+      } else {
+        campo.classList.add('is-valid')
+      }
+    }, false);
 
     });
+  }, false);
+
+  canvas.addEventListener('click', (event) => {
+    canvas.style.borderColor = "#dfe2e6";
+    document.getElementById("isFirmaValida").classList.remove("d-block");
   }, false);
 })()
 
 // Función que se ejecutará el presionar el botón de enviar
-function enviar(){
+let enviar = () => {
   // Todo: validar y marcar con 'is-invalid' los campos vacíos
-  // Ideas para usar el menor código posible entre esto y lo de arriba?
+  var campos = document.getElementsByClassName('form-control');
+  let enviar = true;
+  var validar = Array.prototype.filter.call(campos, (campo) => {
+    if(!campo.checkValidity()){
+      campo.classList.add('is-invalid');
+      enviar = false;
+    }
+    // Si dejan A+ sin seleccionar la lista
+    if(campo.id == 'tsangre')
+      campo.classList.add('is-valid');
+  }, false);
+
+  if(firma.isEmpty()){
+    canvas.style.borderColor = "#d9534f";
+    document.getElementById("isFirmaValida").classList.add("d-block");
+    enviar = false;
+  } else {
+    canvas.style.borderColor = "#5cb85c";
+  }
+
+  if(!enviar) return;
 
   // Todo: enviar los datos mediante XMLHttpRequest o fetch()
   console.log("WIP");
+  const ventana = document.getElementById("modalEnvioDatos");
+  new bootstrap.Modal(ventana).show();
 }
